d3bfd48315e5b288a7df425ed4ca429b98df156a
47pik <pikof47@hotmail.com>
2013-06-27 18:48:24 -0400

    Added 'Done' button to keyboard input

--- src/ca/on/rom/romsearch/MainActivity.java
+++ src/ca/on/rom/romsearch/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
